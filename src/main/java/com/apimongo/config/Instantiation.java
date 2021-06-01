f14dfdec4b5f9012b9d572328c47a8db1d5e357e
weverton Avelino <wtonja@gmail.com>
2021-06-01 14:32:02 -0300

    Referenciando os posts do usuário

--- src/main/java/com/apimongo/config/Instantiation.java
+++ src/main/java/com/apimongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.apimongo.config;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -42,6 +43,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPost().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 		
 	}
 }
--- src/main/java/com/apimongo/domain/User.java
+++ src/main/java/com/apimongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.apimongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 
+	@DBRef(lazy = true)
+	private List<Post> post = new ArrayList<>();
+	
 	public User() {
 	}
 
@@ -46,6 +52,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPost() {
+		return post;
+	}
+	
+	public void setPost(List<Post> post) {
+		this.post = post;
 	}
 
 	@Override
