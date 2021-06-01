4575159c60ea2c31c716ec11a41fe9abd280c8c7
weverton Avelino <wtonja@gmail.com>
2021-06-01 12:45:17 -0300

    inserção de usuario com Post

--- src/main/java/com/apimongo/resource/UserResource.java
+++ src/main/java/com/apimongo/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.apimongo.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.apimongo.domain.User;
 import com.apimongo.dto.UserDto;
@@ -35,6 +39,15 @@ public class UserResource {
 	public ResponseEntity<UserDto> findById(@PathVariable String id){
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDto(obj));
+	}
+
+	@PostMapping
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDto objDto){
+		User obj = userService.fromDto(objDto);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
