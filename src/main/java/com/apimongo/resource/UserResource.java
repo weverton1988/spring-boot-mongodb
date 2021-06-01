9039158172a395cb89a92cef9c452d190db7f43b
weverton Avelino <wtonja@gmail.com>
2021-06-01 13:42:32 -0300

    Atualização de usurio com PUT

--- src/main/java/com/apimongo/resource/UserResource.java
+++ src/main/java/com/apimongo/resource/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -55,6 +56,15 @@ public class UserResource {
 	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<UserDto> delete(@PathVariable String id){
 		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDto objDto, @PathVariable String id){
+		User obj = userService.fromDto(objDto);
+		obj.setId(id);
+		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 	
